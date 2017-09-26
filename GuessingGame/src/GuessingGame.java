86ece036e215d3b8ca60d42b65ee2a2e2b86719f
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2017-09-26 13:06:47 -0400

    Pushing files

---
+++ GuessingGame/src/GuessingGame.java
@@ -0,0 +1,47 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.lang.Math;
+
+/*
+ * Dana Ravvin
+ * Guessing Game
+ * 9/26/17
+ */
+public class GuessingGame {
+
+	public static void main(String [] args)
+	{
+		//computer chooses random number 
+		int randNum = (int) (Math.random()*10);
+		Scanner input = new Scanner(System.in);
+		String s = input.next();
+		int x = input.nextInt();
+		
+		System.out.println("I have chosen a number from 1-10. Can you guess what it is?");
+		
+		while (!(randNum == x))
+		{
+			try
+			{
+				if (randNum == x)
+				{
+					System.out.println("Congrats!");
+				}
+				if (randNum > x)
+				{
+					System.out.println("That's too low! Try again.");
+					 x = input.nextInt();
+				}
+				if (randNum < x)
+				{
+					System.out.println("That's too high! Try again.");
+					 x = input.nextInt();
+				}
+			}
+			catch (InputMismatchException e)
+			{
+				System.out.println("That was error type " + e);
+			}
+		}
+	}
+}
